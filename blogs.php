37b7bb45b2401470caae54ac210b065d3f87644f
AdityaGH2025 <aditya.gupta@aaravtech.net>
2026-07-22 16:58:34 -0400

    chore: update canucks

--- blogs.php
+++ blogs.php
@@ -291,13 +291,6 @@ if ($result === false) {
             style="display:none;visibility:hidden"></iframe>
     </noscript>
 
-    <!-- ✅ Spinner -->
-    <div id="spinner"
-        class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-            <span class="sr-only">Loading...</span>
-        </div>
-    </div>
 
     <!-- Header Top Start -->
     <div class="header-top-section fix">
