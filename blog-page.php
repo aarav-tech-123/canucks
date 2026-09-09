dbabacaec6acbf2ce65ef422cc2a026667f2c93d
AdityaGH2025 <aditya.gupta@aaravtech.net>
2026-09-09 15:32:47 -0400

    chore: update canucks immigration blog

--- blog-page.php
+++ blog-page.php
@@ -95,18 +95,37 @@ if ($result === false) {
     <link rel="stylesheet" href="https://canucksimmigration.com/assets/css/main.css">
     <link rel="stylesheet" href="https://canucksimmigration.com/style.css">
     <!-- Google Tag Manager -->
-    <script>(function (w, d, s, l, i) {
-            w[l] = w[l] || []; w[l].push({
-                'gtm.start':
-                    new Date().getTime(), event: 'gtm.js'
-            }); var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-                    'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-KQSH6WW6');</script>
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-KQSH6WW6');
+    </script>
     <!-- End Google Tag Manager -->
 
     <script
-        type="text/javascript">     (function (c, l, a, r, i, t, y) { c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) }; t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i; y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y); })(window, document, "clarity", "script", "xn0syd8kij"); </script>
+        type="text/javascript">
+        (function(c, l, a, r, i, t, y) {
+            c[a] = c[a] || function() {
+                (c[a].q = c[a].q || []).push(arguments)
+            };
+            t = l.createElement(r);
+            t.async = 1;
+            t.src = "https://www.clarity.ms/tag/" + i;
+            y = l.getElementsByTagName(r)[0];
+            y.parentNode.insertBefore(t, y);
+        })(window, document, "clarity", "script", "xn0syd8kij");
+    </script>
     <style>
         /* ===== UPDATED COLOR SCHEME ===== */
         :root {
@@ -766,6 +785,8 @@ if ($result === false) {
                                             <li>
                                                 <a href="https://www.canucksimmigration.com/contact.html">Contact</a>
                                             </li>
+                                            <li><a href="https://www.canucksimmigration.com/our-immigration-consultant.html">Our Immigration Consultant</a>
+                                            </li>
                                         </ul>
                                     </nav>
                                 </div>
@@ -994,6 +1015,8 @@ if ($result === false) {
                                         Blogs
                                     </a>
                                 </li>
+                                <li><a href="https://www.canucksimmigration.com/our-immigration-consultant.html">Our Immigration Consultant</a></li>
+
                             </ul>
                         </div>
                     </div>
